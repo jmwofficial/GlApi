8e2bcba3c9de17a2bae56a43c55b080c778ee295
jmichaelwilbanks <47291448+jmichaelwilbanks@users.noreply.github.com>
2022-10-21 13:58:03 -0600

    Added API's: +Remove Object From Library +Sever Parent Child Object Relationship

---
+++ GlApi/Controllers/Builder/RemoveObjectFromLibrary.cs
@@ -0,0 +1,27 @@
+﻿using ConceptDbLib;
+using CptClientShared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlApi.Controllers.Builder
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RemoveObjectFromLibrary : ControllerBase
+    {
+        private readonly ILogger<RemoveObjectFromLibrary> _logger;
+        private readonly ConceptDb _cptDb;
+        public RemoveObjectFromLibrary(ILogger<RemoveObjectFromLibrary> logger, ConceptDb cptDb)
+        {
+            _logger = logger;
+            _cptDb = cptDb;
+        }
+        [HttpGet(Name = "RemoveObjectFromLibrary")]
+        public async Task<ConceptDbResponse> Get(string builderId, string libName, string objName)
+        {
+            ConceptDbResponse response = await _cptDb.RemoveObjectFromLibraryAsync(builderId, libName, objName);
+            _logger.LogWarning(ApiMessaging.LogMessage);
+            _logger.LogWarning(ApiMessaging.ResponseToString(response));
+            return response;
+        }
+    }
+}
